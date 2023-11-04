9620bdb27e91f5828de7bb649b1e7f7ca312f758
Ibrahim Khan <116390484+IbrahimKhan2004@users.noreply.github.com>
2023-11-05 00:42:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '⚡Bot By Sahil Nolia⚡'
+    return 'Hello from Koyeb'
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080))
+    app.run()
