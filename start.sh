fa80cb8eb02fa4040c02fc048af7a5ea7998f381
Ibrahim Khan <116390484+IbrahimKhan2004@users.noreply.github.com>
2023-11-04 19:31:59 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,1 @@
-#!/bin/bash
 gunicorn app:app --workers 1 --threads 1 --bind 0.0.0.0:$PORT --timeout 86400 & rclone serve http --addr=0.0.0.0:$PORT --config=rclone.conf $DRIVE_NAME: & python3 update.py & python3 main.py
